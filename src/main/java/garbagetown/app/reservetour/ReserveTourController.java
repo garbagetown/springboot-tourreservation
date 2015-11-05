c7c93af4b8560ea07762d41ebaddb6deb23cf16e
garbagetown <garbagetown@gmail.com>
2015-11-06 00:51:51 +0900

    add reservationh confirm page

--- src/main/java/garbagetown/app/reservetour/ReserveTourController.java
+++ src/main/java/garbagetown/app/reservetour/ReserveTourController.java
@@ -5,6 +5,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.web.bind.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -22,7 +24,7 @@ public class ReserveTourController {
     @Inject
     ReserveTourHelper reserveTourHelper;
 
-    @RequestMapping(value = "{tourCode}", method = RequestMethod.GET, params = "form")
+    @RequestMapping(value = {"{tourCode}", "{tourCode}/reserve"}, method = RequestMethod.GET, params = "form")
     public String reserveForm(@AuthenticationPrincipal ReservationUserDetails userDetails,
                               @PathVariable("tourCode") String tourCode, ReserveTourForm form, Model model) {
 
@@ -31,5 +33,44 @@ public class ReserveTourController {
         model.addAttribute("output", output);
 
         return "reservetour/reserveForm";
+    }
+
+    @RequestMapping(value = "{tourCode}/reserve", method = RequestMethod.POST, params = "confirm")
+    public String confirm(@AuthenticationPrincipal ReservationUserDetails userDetails,
+                          @PathVariable("tourCode") String tourCode, @Validated ReserveTourForm form,
+                          BindingResult result, Model model) {
+
+        if (result.hasErrors()) {
+            return reserveForm(userDetails, tourCode, form, model);
+        }
+
+        TourDetailOutput output = reserveTourHelper.findTourDetail(userDetails, tourCode, form);
+
+        model.addAttribute("output", output);
+
+        return "reservetour/reserveConfirm";
+    }
+
+    @RequestMapping(value = "{tourCode}/reserve", method = RequestMethod.POST)
+    public String reserve(@AuthenticationPrincipal ReservationUserDetails userDetails,
+                          @PathVariable("tourCode") String tourCode, @Validated ReserveTourForm form,
+                          BindingResult result, Model model) {
+
+        if (result.hasErrors()) {
+            return reserveForm(userDetails, tourCode, form, model);
+        }
+        return "redirect:/tours/{tourCode}/reserve?complete";
+    }
+
+    @RequestMapping(value = "{tourCode}/reserve", method = RequestMethod.GET, params = "complete")
+    public String reserveComplete() {
+        return "reservetour/reserveComplete";
+    }
+
+    @RequestMapping(value = "{tourCode}/reserve", method = RequestMethod.POST, params = "redo")
+    public String reserveRedo(@AuthenticationPrincipal ReservationUserDetails userDetails,
+                              @PathVariable("tourCode") String tourCode,
+                              ReserveTourForm form, Model model) {
+        return reserveForm(userDetails, tourCode, form, model);
     }
 }
