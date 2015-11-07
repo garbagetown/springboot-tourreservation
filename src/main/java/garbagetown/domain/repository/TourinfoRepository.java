bbf51f84da57fdb815cbda8623b52f8be84122c4
garbagetown <garbagetown@gmail.com>
2015-11-07 16:36:19 +0900

    add findOneForUpdate

--- src/main/java/garbagetown/domain/repository/TourinfoRepository.java
+++ src/main/java/garbagetown/domain/repository/TourinfoRepository.java
@@ -2,9 +2,18 @@ package garbagetown.domain.repository;
 
 import garbagetown.domain.model.Tourinfo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import javax.persistence.LockModeType;
 
 /**
  * Created by garbagetown on 10/15/15.
  */
 public interface TourinfoRepository extends JpaRepository<Tourinfo, String>, TourinfoRepositoryCustom {
+
+    @Query("SELECT t FROM Tourinfo t WHERE t.tourCode = :tourCode")
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    Tourinfo findOneForUpdate(@Param("tourCode") String tourCode);
 }
