79acc663ebe1d737e8af0f74e1bb9df52a03659c
garbagetown <garbagetown@gmail.com>
2015-11-23 15:54:29 +0900

    add tests for countReservedPersonSumByTourinfo

--- src/test/java/garbagetown/domain/repository/reserve/ReserveRepositoryImplTest.java
+++ src/test/java/garbagetown/domain/repository/reserve/ReserveRepositoryImplTest.java
@@ -3,7 +3,9 @@ package garbagetown.domain.repository.reserve;
 import garbagetown.SpringbootTourreservationApplication;
 import garbagetown.domain.model.Customer;
 import garbagetown.domain.model.Reserve;
+import garbagetown.domain.model.Tourinfo;
 import garbagetown.domain.repository.reserve.ReserveRepository;
+import org.joda.time.DateTime;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
@@ -14,10 +16,14 @@ import org.springframework.test.context.transaction.TransactionConfiguration;
 
 import javax.inject.Inject;
 import javax.transaction.Transactional;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.CoreMatchers.nullValue;
 import static org.junit.Assert.assertThat;
 
 /**
@@ -47,5 +53,67 @@ public class ReserveRepositoryImplTest {
 
         assertThat(reservationList, is(notNullValue()));
         assertThat(reservationList.size(), is(0));
+    }
+
+    @Test
+    public void testCountReservedPersonSumByTourinfo() {
+
+        Tourinfo tourinfo = new Tourinfo();
+        tourinfo.setTourCode("0000000001");
+
+        Customer customer = new Customer();
+        customer.setCustomerCode("00000001");
+
+        Reserve reserve = new Reserve();
+        reserve.setReserveNo("10000001");
+        reserve.setTourinfo(tourinfo);
+        reserve.setReservedDay(DateTime.now().toDate());
+        reserve.setAdultCount(1);
+        reserve.setChildCount(2);
+        reserve.setCustomer(customer);
+        reserve.setTransfer("0");
+        reserve.setSumPrice(1000);
+        reserve.setRemarks("test");
+        updateReserve(reserve);
+
+        reserve.setReserveNo("10000002");
+        reserve.setAdultCount(3);
+        reserve.setChildCount(4);
+        updateReserve(reserve);
+
+        Long count = reserveRepository.countReservedPersonSumByTourinfo(tourinfo);
+
+        assertThat(count, is(1 + 2 + 3 + 4L));
+    }
+
+    @Test
+    public void testCountReservedPersonSumByTourinfo_notFound() {
+
+        Tourinfo tourinfo = new Tourinfo();
+        tourinfo.setTourCode("9999999999");
+
+        Long count = reserveRepository.countReservedPersonSumByTourinfo(tourinfo);
+
+        assertThat(count, is(nullValue()));
+    }
+
+    private void updateReserve(Reserve reserve) {
+        String sql = "INSERT INTO reserve(" +
+                "reserve_no, tour_code, reserved_day, adult_count, child_count, customer_code, transfer, sum_price, remarks" +
+                ") VALUES (" +
+                ":reserveNo, :tourCode, :reservedDay, :adultCount, :childCount, :customerCode, :transfer, :sumPrice, :remarks)";
+
+        Map<String, Object> paramMap = new HashMap<String, Object>();
+        paramMap.put("reserveNo", reserve.getReserveNo());
+        paramMap.put("tourCode", reserve.getTourinfo().getTourCode());
+        paramMap.put("reservedDay", reserve.getReservedDay());
+        paramMap.put("adultCount", reserve.getAdultCount());
+        paramMap.put("childCount", reserve.getChildCount());
+        paramMap.put("customerCode", reserve.getCustomer().getCustomerCode());
+        paramMap.put("transfer", reserve.getTransfer());
+        paramMap.put("sumPrice", reserve.getSumPrice());
+        paramMap.put("remarks", reserve.getRemarks());
+
+        jdbcTemplate.update(sql, paramMap);
     }
 }
