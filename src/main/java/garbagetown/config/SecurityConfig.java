54ec7f0c67aef057f7b974aea2c5d91b1eb5d5da
garbagetown <garbagetown@gmail.com>
2015-11-29 15:38:12 +0900

    modify slightly to access the h2 console. fix #6

--- src/main/java/garbagetown/config/SecurityConfig.java
+++ src/main/java/garbagetown/config/SecurityConfig.java
@@ -34,6 +34,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .logoutSuccessUrl("/")
                 .deleteCookies("JSESSIONID")
                 .invalidateHttpSession(true);
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
     }
 
     @Override
